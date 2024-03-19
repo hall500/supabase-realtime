6cf4ef9a7fd1923971023ab351f5aa1ce8e6fae6
Abdullah Momoh <momohabdullah20@gmail.com>
2024-03-19 07:42:36 +0100

    updated blocknote js

--- src/components/Blocknote.js
+++ src/components/Blocknote.js
@@ -3,58 +3,34 @@ import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/react/style.css";
 import { useEffect, useRef, useState } from "react";
 import "../styles.css";
-import { ROOM, channel } from "../supabase";
-import { faker } from '@faker-js/faker';
-import * as Y from "yjs";
-import { WebrtcProvider } from "y-webrtc";
+import { channel } from '../supabase';
 
 export default function Blocknote() {
   const [blocks, setBlocks] = useState([]);
   const subscriptionRef = useRef(null);
-  const docRef = useRef(null);
-  const providerRef = useRef(null);
-
-  useEffect(() => {
-    if (!docRef?.current) { // Check if not already subscribed
-      console.log('creating document');
-      docRef.current = new Y.Doc({ collectionid: ROOM });
-    }
-    
-    if (!providerRef?.current) { // Check if not already subscribed
-      console.log('creating provider');
-      providerRef.current = new WebrtcProvider(docRef.current);
-    }
-
-  });
-
-  useEffect(() => {
-    if (!subscriptionRef?.current?.joinedOnce) { // Check if not already subscribed
-      subscriptionRef.current = channel;
-      subscriptionRef.current.on('broadcast', { event: 'test' }, ({ payload }) =>{
-
-      }).subscribe();
-    }
-  });
-
-  const collabo = (providerRef.current) ? {
-    // The Yjs Provider responsible for transporting updates:
-    provider: providerRef.current,
-    // Where to store BlockNote data in the Y.Doc:
-    fragment: docRef.current?.getXmlFragment("document-store"),
-    // Information (name and color) for this user:
-    user: {
-      name: faker.person.fullName(),
-      color: "#ff0000",
-    },
-  } : {};
 
   const editor = useCreateBlockNote({
-    collaboration: collabo,
   });
 
+  useEffect(() => {
+    // Subscribe to the Channel
+    if (!subscriptionRef?.current?.joinedOnce) {
+      subscriptionRef.current = channel;
+      channel.on('broadcast', { event: 'test' }, ({ payload }) => {
+          setBlocks(payload.blocks);
+        })
+        .subscribe();
+    }
+  
+    // Clean up subscription on component unmount
+    return () => {
+      subscriptionRef.current?.unsubscribe();
+    };
+  }, []);
+
   const handleEditorChange = () => {
-    //const block = editor.getTextCursorPosition().block;
-    setBlocks(editor.document);
+    const blocks = editor.document;
+    setBlocks(blocks);
     // Send a message once the client is subscribed
     if(subscriptionRef.current) subscriptionRef.current.send({
       type: 'broadcast',
